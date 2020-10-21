08fd04b40b917abd277934f5080a1a3369879f86
Isay Castaneda <isay.castaneda@bowheadhealth.com>
2020-10-21 18:46:22 -0500

    add new data

--- dataset_validation.py
+++ dataset_validation.py
@@ -1,9 +1,11 @@
 import pandas as pd
 
 JSON_FILE = "Arms Up.json"
-labels = ['', 'down']
+labels = ['up', 'down']
+
 
 validation_df = pd.read_json(JSON_FILE, orient='records')
+#print(validation_df[~validation_df.isna()])
 validation_df['label'] = validation_df['label'].replace(labels, [0, 1])
 validation_df.to_json('validationArmsUp.json', orient='records' )
 
