bf28f62d720a844d58d70304690d4b467c25f08e
Isay Castaneda <isay.castaneda@bowheadhealth.com>
2020-10-21 21:36:01 -0500

    add squat data

--- dataset_validation.py
+++ dataset_validation.py
@@ -1,13 +1,16 @@
 import pandas as pd
 
-JSON_FILE = "./rawcsv/jumpinJacks.json"
+JSON_FILE = "./rawcsv/squat.json"
 labels = ['up', 'down']
-jsonValidatedName = 'validationJumpingJacks.json'
+jsonValidatedName = 'validationSquat.json'
 
 
 validation_df = pd.read_json(JSON_FILE, orient='records')
-if (len(validation_df.index) != len(validation_df[~validation_df.isna()].index)):
-    validation_df = validation_df[~validation_df.isna()]
+# if (len(validation_df.index) != len(validation_df[~validation_df.isnull()].index)):
+#     validation_df = validation_df[~validation_df.isna()]
+
+print(validation_df)
+print(validation_df[~validation_df.isna()])
 
 validation_df['label'] = validation_df['label'].replace(labels, [0, 1])
 validation_df.to_json(jsonValidatedName, orient='records' )
