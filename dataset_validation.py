d57a4e4263a6101cd7e9496b70f4fa9de477b322
Isay Castaneda <isay.castaneda@bowheadhealth.com>
2020-10-21 23:16:56 -0500

    added

--- dataset_validation.py
+++ dataset_validation.py
@@ -1,17 +1,16 @@
 import pandas as pd
 
-JSON_FILE = "./rawcsv/Pose match.json"
-labels = ['Y', 'M', 'C']
-jsonValidatedName = 'validationPoseMatch.json'
+JSON_FILE = "./rawcsv/jumpinJacks.json"
+labels = ['up', 'down']
+jsonValidatedName = 'validationJumpingJacks.json'
 
 
 validation_df = pd.read_json(JSON_FILE, orient='records')
-if (len(validation_df.index) != len(validation_df[~validation_df.isnull()].index)):
-     validation_df = validation_df[~validation_df.isna()]
 
-validation_df = validation_df[~pd.isna(validation_df.rightElbowAngle)]
 
-validation_df['label'] = validation_df['label'].replace(labels, [0, 1, 2])
-validation_df.to_json(jsonValidatedName, orient='records' )
+validation_df = validation_df[(~pd.isna(validation_df.rightElbowAngle)) & (~pd.isna(validation_df.leftElbowAngle))]
+
+validation_df['label'] = validation_df['label'].replace(labels, [0, 1])
+validation_df.to_json(jsonValidatedName, orient='records')
 
 
