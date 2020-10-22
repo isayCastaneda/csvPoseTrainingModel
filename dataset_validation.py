cb461639f44787e072c6a17cf5fd8ea506cb65c9
Isay Castaneda <isay.castaneda@bowheadhealth.com>
2020-10-21 19:36:21 -0500

    add jumping jacks data

--- dataset_validation.py
+++ dataset_validation.py
@@ -1,12 +1,15 @@
 import pandas as pd
 
-JSON_FILE = "Arms Up.json"
-labels = ['up', 'down']
+JSON_FILE = "./rawcsv/jumpinJacks.json"
+labels = ['', 'down']
+jsonValidatedName = 'validationJumpingJacks.json'
 
 
 validation_df = pd.read_json(JSON_FILE, orient='records')
-#print(validation_df[~validation_df.isna()])
+if (len(validation_df.index) != len(validation_df[~validation_df.isna()].index)):
+    validation_df = validation_df[~validation_df.isna()]
+
 validation_df['label'] = validation_df['label'].replace(labels, [0, 1])
-validation_df.to_json('validationArmsUp.json', orient='records' )
+validation_df.to_json(jsonValidatedName, orient='records' )
 
 
